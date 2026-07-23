93ccbb70ba30b2b1a31a13342c1c69c6951cd25a
Hai Vu <haivu2004@gmail.com>
2026-07-22 19:54:59 -0700

    feat: add help and version to argparse parser

--- src/hier/__main__.py
+++ src/hier/__main__.py
@@ -1,4 +1,14 @@
+"""
+Prints hierarchical data.
+
+This data can be JSON, TOML, XML, YAML, or even a file system's directory.
+
+- If the path is a directory, prints its content like that of the tree command
+- If the path points to a JSON, TOML, XML, or YAML file, prints its content
+"""
+
 import argparse
+import importlib.metadata
 import json
 import pathlib
 import tomllib
@@ -8,10 +18,24 @@ import yaml
 
 from . import print_tree
 
+CLI_NAME = "hier"
+
+try:
+    __version__ = importlib.metadata.version(CLI_NAME)
+except importlib.metadata.PackageNotFoundError:
+    __version__ = "0.0.0-dev"
+
 
 def main():
-    parser = argparse.ArgumentParser(prog="ptree")
+    parser = argparse.ArgumentParser(
+        prog=CLI_NAME,
+        description=__doc__,
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+    )
     parser.add_argument("path")
+    parser.add_argument(
+        "-V", "--version", action="version", version=f"{CLI_NAME} v{__version__}"
+    )
     options = parser.parse_args()
 
     path = pathlib.Path(options.path)
