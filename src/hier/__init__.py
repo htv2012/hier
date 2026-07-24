b4877be2b42a602dee38f6eefcf4fe7e86598223
Hai Vu <haivu2004@gmail.com>
2026-07-24 08:31:57 -0700

    feat: implement hier as a replacement to print_hier

--- src/hier/__init__.py
+++ src/hier/__init__.py
@@ -1,7 +1,102 @@
+import contextlib
 import functools
 import pathlib
 import xml.etree.ElementTree as ET
 from typing import Optional, TextIO
+
+
+def element_tree_iterator(data):
+    root = data.getroot()
+    return list(root)
+
+
+def get_iterator(data):
+    if isinstance(data, list):
+        return list
+    elif isinstance(data, dict):
+        return dict.items
+    elif isinstance(data, pathlib.Path):
+        return pathlib.Path.iterdir
+    elif isinstance(data, ET.Element):
+        return list
+    elif isinstance(data, ET.ElementTree):
+        return element_tree_iterator
+    raise TypeError(f"cannot handle data type {data.__class__.__name__}")
+
+
+def fmt_root(data):
+    if isinstance(data, pathlib.Path):
+        return str(data)
+    elif isinstance(data, ET.ElementTree):
+        root = data.getroot()
+        return root.tag
+    return ""
+
+
+def get_child(entry):
+    if isinstance(entry, tuple):
+        return entry[1]  # dict
+    return entry
+
+
+def is_container(entry) -> bool:
+    if isinstance(entry, ET.Element):
+        return list(entry) != []
+    elif isinstance(entry, (list, dict)):
+        return True
+    elif isinstance(entry, pathlib.Path):
+        with contextlib.suppress(NotADirectoryError):
+            return list(entry.iterdir()) != []
+        return False
+    return False
+
+
+def fmt_node(index, entry) -> str:
+    if isinstance(entry, tuple) and len(entry) == 2:
+        return f"{entry[0]}={entry[1]!r}"
+    elif isinstance(entry, ET.Element):
+        value = entry.text.strip()
+        value = f"={value!r}" if value else ""
+        return f"{entry.tag}{value}"
+    elif isinstance(entry, pathlib.Path):
+        return entry.name
+
+    # must be a list entry if gets this far
+    child = get_child(entry)
+    if is_container(child):
+        return f"[{index}]"
+    else:
+        return f"[{index}]={entry}"
+
+
+def hier(data, prefix: str = "", file: Optional[TextIO] = None):
+    # print just the root and let _hier handle the children
+    root = fmt_root(data)
+    if root:
+        print(f"{prefix}{root}", file=file)
+    _hier(data, prefix, file)
+
+
+def _hier(data, prefix: str = "", file: Optional[TextIO] = None):
+    iterator = get_iterator(data)
+    children = list(iterator(data))
+    count = len(children)
+
+    for i, entry in enumerate(children):
+        is_last = i == count - 1
+        connector = "└── " if is_last else "├── "
+
+        child = get_child(entry)
+        formatted_node = fmt_node(i, entry)
+        print(f"{prefix}{connector}{formatted_node}", file=file)
+        if is_container(child):
+            extension = "    " if is_last else "│   "
+            _hier(child, prefix + extension)
+
+
+# ======================================================================
+# Old code, to be removed
+# ======================================================================
 
 
 @functools.singledispatch
--- src/hier/sandbox.py
+++ src/hier/sandbox.py
@@ -1,6 +1,13 @@
 import pathlib
 import xml.etree.ElementTree as ET
 
+from hier import hier
+
+
+def element_tree_iterator(data):
+    root = data.getroot()
+    return list(root)
+
 
 def get_iterator(data):
     if isinstance(data, list):
@@ -11,17 +18,21 @@ def get_iterator(data):
         return pathlib.Path.iterdir
     elif isinstance(data, ET.Element):
         return list
+    elif isinstance(data, ET.ElementTree):
+        return element_tree_iterator
+    raise TypeError(f"cannot handle data type {data.__class__.__name__}")
 
 
 objects = [
     dict(ab=1, cd=2, ef=3),
-    ["peter", "paul", "mary"],
+    ["peter", "paul", "mary", ["one", "two"]],
     pathlib.Path("src/hier"),
-    ET.fromstring("<details><uid>501</uid><alias>Anna</alias></details>"),
+    ET.parse("samples/kens_books.xml"),
 ]
 
 for obj in objects:
-    iterator = get_iterator(obj)
-    for entry in iterator(obj):
-        print(entry)
-    print("=" * 80)
+    print()
+    print("# =========================================================================")
+    print(f"# {obj.__class__.__name__}")
+    print("# =========================================================================")
+    hier(obj)
